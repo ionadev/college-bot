8ec9ad60722310e878e6618363977dc6976720fe
Piyush Bhangale <bhangalepiyush@gmail.com>
2018-08-17 11:52:44 +0530

    Fixes

---
+++ commands/Admin/update.js
@@ -0,0 +1,24 @@
+const { Command, util: { exec, codeBlock } } = require("klasa");
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            aliases: ["pull"],
+            description: "-BOT OWNER ONLY-",
+            guarded: true,
+            permissionLevel: 10,
+           
+        });
+    }
+
+    async run(msg) {
+        const result = await exec('git pull', { timeout: "timeout" in msg.flags ? Number(msg.flags.timeout) : 60000 })
+            .catch(error => ({ stdout: null, stderr: error }));
+        const output = result.stdout ? `**\`OUTPUT\`**${codeBlock("prolog", result.stdout)}` : "";
+        const outerr = result.stderr ? `**\`ERROR\`**${codeBlock("prolog", result.stderr)}` : "";
+
+        return msg.sendMessage([output, outerr].join("\n"));
+    }
+
+};
